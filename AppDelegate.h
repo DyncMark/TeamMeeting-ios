0dbecc5fe200a1cb5ac763d17783c88b65ea532c
wangyangyang@dync.cc <wangyangyang@dync.cc>
2015-12-14 15:17:25 +0800

    add file

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eagle
+//
+//  Created by liu on 15/9/21.
+//  Copyright (c) 2015年 tianchuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class TCNavigationController;
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) TCNavigationController *viewController;
+@end
+
