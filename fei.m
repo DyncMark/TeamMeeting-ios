bf96d65abdf8d9fe6194b1b684d59c90f882635f
wangyangyang@dync.cc <wangyangyang@dync.cc>
2015-12-16 09:45:41 +0800

    add file

---
+++ fei.m
@@ -0,0 +1,38 @@
+//
+//  fei.m
+//  abc
+//
+//  Created by yangyang on 15/10/1.
+//  Copyright © 2015年 yangyang. All rights reserved.
+//
+
+#import "fei.h"
+
+@implementation Fei
+
+
+- (BOOL) takephone:(int)phonenumber{
+    
+
+    int ab = 100 ;
+    
+    
+
+    int c  =8 ;
+    
+    int de =90 ;
+
+
+
+
+
+
+
+
+
+
+
+
+    return YES;
+}
+@end
